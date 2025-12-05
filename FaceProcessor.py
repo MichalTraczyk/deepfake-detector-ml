d0cf02db08976234fe89c14775d194c4a1ac3a04
Michał Traczyk <michal.traczyk02@proton.me>
2025-12-05 22:53:53 +0100

    Correct face processor

--- FaceProcessor.py
+++ FaceProcessor.py
@@ -20,34 +20,12 @@ class ImageFaceProcessor:
         tenengrad_score = np.mean(sobel_magnitude)
         return laplacian_var < lap_threshold or tenengrad_score < ten_threshold
 
-    def align_face(self, image, landmarks):
-        # Align based on eye centers
-        left_eye = np.mean(landmarks[36:42], axis=0)
-        right_eye = np.mean(landmarks[42:48], axis=0)
-
-        dx = right_eye[0] - left_eye[0]
-        dy = right_eye[1] - left_eye[1]
-        angle = np.degrees(np.arctan2(dy, dx))
-
-        eyes_center = ((left_eye[0] + right_eye[0]) / 2,
-                       (left_eye[1] + right_eye[1]) / 2)
-
-        rot_matrix = cv2.getRotationMatrix2D(eyes_center, angle, scale=1.0)
-        aligned = cv2.warpAffine(image, rot_matrix, (image.shape[1], image.shape[0]), flags=cv2.INTER_LINEAR)
-
-        return aligned
     def get_face(self, image):
-        preds = self.aligner.get_landmarks(image)
-        if preds is None:
-            return None
-        landmarks = preds[0]
-        aligned_image = self.align_face(image, landmarks)
-
         face_locations = face_recognition.face_locations(image)
         if not face_locations:
             return None
         top, right, bottom, left = face_locations[0]
-        face = aligned_image[top:bottom, left:right]
+        face = image[top:bottom, left:right]
         face = cv2.resize(face, self.resolution)
         if self.is_blurry(face):
             return None
