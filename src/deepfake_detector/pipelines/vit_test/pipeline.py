48b35130bfffb0d03ae538249dec0e3b4b976fba
Kamolus <kamilpanka06@gmail.com>
2026-01-11 20:28:58 +0100

    Fixing test pipeline for ViT model

--- src/deepfake_detector/pipelines/vit_test/pipeline.py
+++ src/deepfake_detector/pipelines/vit_test/pipeline.py
@@ -1,35 +1,24 @@
 from kedro.pipeline import Pipeline, node, pipeline
-from .test import create_vit_gradcam_visualization, get_test_model, get_test_dataloader, run_evaluation
-
+from .test import load_vit_model_node, create_test_dataloader_node, create_vit_gradcam_plot_node
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
         node(
-            func=get_test_dataloader,
-            inputs=["params:learning_settings"],
-            outputs="test_dataloader",
-            name="loaders_node"
+            func=create_test_dataloader_node,
+            inputs="params:learning_settings",
+            outputs="vit_test_loader",
+            name="create_vit_dataloader_node",
         ),
         node(
-            func=get_test_model,
-            inputs=["params:learning_settings","params:vit_model","params:paths"],
-            outputs="test_model",
-            name="model_node"
+            func=load_vit_model_node,
+            inputs=["params:vit_model", "params:paths", "params:learning_settings"],
+            outputs="loaded_vit_model",
+            name="load_vit_model_node",
         ),
         node(
-            func=run_evaluation,
-            inputs=["test_model", "test_dataloader"],
-            outputs="final_metrics_vit"
+            func=create_vit_gradcam_plot_node,
+            inputs=["loaded_vit_model", "vit_test_loader"],
+            outputs="vit_gradcam_plot",
+            name="create_vit_gradcam_visualization_node",
         ),
-        # node(
-        #     func=create_vit_gradcam_visualization,
-        #     inputs=[
-        #         "test_model",
-        #         "test_dataloader",
-        #         "params:learning_settings",
-        #         "params:vit_model"
-        #     ],
-        #     outputs="vit_gradcam_image",
-        #     name="generate_vit_gradcam_node"
-        # )
     ])
